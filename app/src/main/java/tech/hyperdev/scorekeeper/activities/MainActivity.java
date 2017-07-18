b720f454f0d4e7ca61e7b2a681f437b837a2e1ee
kimberlycanedo12 <kimberycanedo02@gmail.com>
2017-07-19 01:27:07 +0800

    (3) Modify team names of each score fragment.

--- app/src/main/java/tech/hyperdev/scorekeeper/activities/MainActivity.java
+++ app/src/main/java/tech/hyperdev/scorekeeper/activities/MainActivity.java
@@ -1,26 +1,75 @@
 package tech.hyperdev.scorekeeper.activities;
 
+import android.os.PersistableBundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import tech.hyperdev.scorekeeper.R;
 import tech.hyperdev.scorekeeper.fragments.ScoreFragment;
 
 public class MainActivity extends AppCompatActivity {
+    int styleId = R.style.AppTheme;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        if(savedInstanceState != null){
+            styleId = savedInstanceState.getInt("Theme");
+        }
+        setTheme(styleId);
         setContentView(R.layout.activity_main);
 
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        ScoreFragment firstTeam = new ScoreFragment();
-        ScoreFragment secondTeam = new ScoreFragment();
-        fragmentTransaction.add(R.id.fragment_container, firstTeam, "First Team");
-        fragmentTransaction.add(R.id.fragment2_container, secondTeam, "Second Team");
-        fragmentTransaction.commit();
+
+
+        if (savedInstanceState == null) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            ScoreFragment firstTeam = new ScoreFragment("Team 1", 0);
+            ScoreFragment secondTeam = new ScoreFragment("Team 2", 0);
+            fragmentTransaction.add(R.id.fragment_container, firstTeam, "First Team");
+            fragmentTransaction.add(R.id.fragment2_container, secondTeam, "Second Team");
+            fragmentTransaction.commit();
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("Theme", styleId);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        super.onOptionsItemSelected(item);
+
+        switch (item.getItemId()) {
+            case R.id.dayMode:
+                styleId = R.style.AppTheme;
+                recreate();
+                break;
+            case R.id.nightMode:
+                styleId = R.style.AppTheme_Night;
+                recreate();
+                break;
+        }
+        return true;
+
     }
 }
--- app/src/main/java/tech/hyperdev/scorekeeper/fragments/ScoreFragment.java
+++ app/src/main/java/tech/hyperdev/scorekeeper/fragments/ScoreFragment.java
@@ -5,23 +5,78 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import tech.hyperdev.scorekeeper.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ScoreFragment extends Fragment {
+public class ScoreFragment extends Fragment implements View.OnClickListener {
+
+    private TextView titleTextView, scoreTextView;
+    private ImageButton plusButton, minusButton;
+
+    private String teamName;
+    private int score;
 
     public ScoreFragment() {
-        // Required empty public constructor
+
+    }
+
+    public ScoreFragment(String teamName, int score) {
+        this.teamName = teamName;
+        this.score = score;
+    }
+
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("TeamName", teamName);
+        outState.putInt("Score", score);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_score, container, false);
+        View view = inflater.inflate(R.layout.fragment_score, container, false);
+
+        titleTextView = (TextView) view.findViewById(R.id.tvTeamName);
+        scoreTextView = (TextView) view.findViewById(R.id.textView2);
+        plusButton = (ImageButton) view.findViewById(R.id.btnPlus);
+        minusButton = (ImageButton) view.findViewById(R.id.btnMinus);
+
+        if (savedInstanceState != null) {
+            teamName = savedInstanceState.getString("TeamName");
+            score = savedInstanceState.getInt("Score");
+        }
+        titleTextView.setText(teamName);
+        scoreTextView.setText(Integer.toString(score));
+
+
+        plusButton.setOnClickListener(this);
+
+
+        minusButton.setOnClickListener(this);
+
+        return view;
     }
 
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.btnPlus) {
+            score++;
+            scoreTextView.setText(Integer.toString(score));
+        } else if (view.getId() == R.id.btnMinus) {
+            if (score > 0) {
+                score--;
+                scoreTextView.setText(Integer.toString(score));
+            }
+        }
+    }
 }
